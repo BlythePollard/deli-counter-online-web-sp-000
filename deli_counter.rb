b9bce33bc14bfc1af8079291da7405faa2b94c4d
Blythe Pollard <blytheepollard@gmail.com>
2019-09-11 19:26:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,7 @@ end
 end
 
 def take_a_number(katz_deli, name)
+<<<<<<< HEAD
     one_in_line = katz_deli << "#{name}"
     last_place = katz_deli.length
     puts "Welcome, #{name}. You are number #{last_place} in line."
@@ -29,3 +30,27 @@ end
 
 
 
+=======
+  if katz_deli.length == 0  
+    one_in_line = katz_deli << "#{name}"
+    puts "Welcome, #{name}. You are number 1 in line."
+  elsif katz_deli.length > 0
+    katz_deli << "#{name}"
+    katz_deli.each_with_index do |name, index|
+      many_in_line = katz_deli << "Welcome, #{name}, you are number #{index} in line."
+end
+puts many_in_line
+end
+end
+
+#katz_deli.each_with_index do |name, index| 
+   # puts "Welcome, #{name}. You are number #{index + 1} in line."
+    
+    
+  #"The line is currently: 1. Logan 2. Avi"
+  #returns array to string with The line is currently [array index] + . #{name}
+
+def now_serving(katz_deli)
+  #puts next person in line
+end
+>>>>>>> 4963f0e5ebada48502a3f3148d19e167363d0528
